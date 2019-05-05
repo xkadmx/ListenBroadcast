cfa4d42af4ee81c907f92ca767bf4acbd44f5895
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-05 12:07:31 +0200

    setting Toast arguments to the makeText()

--- app/src/main/java/com/example/listenbroadcast/ReceiveBroadcast.java
+++ app/src/main/java/com/example/listenbroadcast/ReceiveBroadcast.java
@@ -9,8 +9,5 @@ public class ReceiveBroadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+        Toast.makeText(context,"Broadcast received!", Toast.LENGTH_LONG);
 }
